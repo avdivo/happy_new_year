f0d3311ac3af754b57bf62f265f589cf13fd335e
avdivo <avdivo@mail.ru>
2022-12-30 02:02:48 +0300

    Начало обмена с бэком

--- happy_new_year/views.py
+++ happy_new_year/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     """ Главная страница, без основного подарка """
     return render(request, 'index.html', locals())
+
+
+def prediction(request):
+    """ Выбор предсказания для Ajax запроса по присланному номеру подарка """
+    print(request, '------------------------------')
+    return render(request, 'index.html', locals())
---
+++ static/js/script.js
@@ -0,0 +1,62 @@
+// Функции скрипта:
+// 1. Отправляет номер подарка, получает текст предсказания
+// 2. Показывает пересланное сообщение при щелчке на большом подарке
+// 3. Отправляет текст сообщения, получает ссылку, чтоб его показать
+
+$(document).ready(function(){
+    // Установка ширины div для вывода текста в рамке
+    let w = $('#img_frame').width();
+    $('.frame_div').width(w/1.8);
+
+    // Ajax
+    function server(form){
+            // Функция для отправки формы и номера подарка
+            // и получения ссылки на подарок и текста предскаания
+            var url = form.attr("action");
+            var data = form.serialize(); // Отправка формы в виде словаря
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: data,
+                cache: true,
+                success: function (data) {
+                    // Операция успешно выполнена
+                    if (change == 'delete'){
+                        // Удаляем кандидатов на удаление (на сервере это уже сделано)
+                        $.each(product, function(i, del) {
+                            $('#row_' + del).remove();
+                        });
+                    }
+                        $('#exampleModal').modal('show'); // Показать сообщение о добавлении товара в корзину
+                        // Сурываем сообщение о добавлении товара в корзину
+                        setTimeout(function(){
+                            $('#exampleModal').modal('hide');
+                        }, 1100);
+
+                        // Изменяем отображаемое значение на корзине
+                        b = (data.products == 0) ? '' : data.products;
+                        $('#quantity_in_basket').text(b);
+                        updatePage();
+                },
+                error: function(){
+                    console.log('error');
+                    // Произошла ошибка связи с сервером или обновления данных
+                    if (change == 'change') {
+                        // Отменяем добавление или отнятие товаров в корзину
+                        $('#var-value_' + product).html($('#sum_' + product).html() / $('#price_' + product).html());
+                    }
+                    alert('Ошибка при передаче данных!');
+                    updatePage();
+                }
+            });
+
+
+    }
+
+    // Выбран подарок с предсказанием
+    $("[name = 'gift']").on('click', function(e){
+        $('#number_gift').val($(this).attr("class")[$(this).attr("class").length - 1])  // Номер подарка узнаем из имени класса
+        server($('#form_prediction'))
+    });
+
+});
